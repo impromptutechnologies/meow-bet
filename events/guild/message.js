3e3b044a50a7a7976d0d260de2c2ab58c5b8a308
Jongbin Won <jongbinwon@192.168.2.4>
2021-07-02 13:41:59 +0700

    d

--- events/guild/message.js
+++ events/guild/message.js
@@ -21,7 +21,7 @@ module.exports = async (Discord, client, message) => {
       .setColor("#304281")
       .setTitle(`Welcome to Meow, ${message.author.username}!`)
       .setThumbnail(`https://altvaton.sirv.com/Images/heart.png`)
-      .setDescription(`Our bot allows you to bet on major sports matches, stock/crypto prices, and a host of cool casino games such as blackjack, dice, slots and more! We give you 100 free tokens a day or you can purchase a lootbox containing tokens, premium commands and the monthly giveaway/lottery. 
+      .setDescription(`Our bot allows you to bet on major sports matches, stock/crypto prices, and a host of cool casino games such as blackjack, dice, slots and more! We give you 1000 free tokens or you can purchase a lootbox containing tokens, premium commands and the monthly giveaway/lottery. 
       \nYou can access the list of commands on our website ([meowmeow.gg/bets](https://getmeow.gg/bets)) along with the latest events you can bet on.`)
       .setURL("https://getmeow.gg/bets");
       message.author.send(newEmbed)
