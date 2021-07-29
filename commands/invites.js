b54d3bab1e3269929b2c01c7ed44d4d38734df90
Jongbin Won <jongbinwon@192.168.2.4>
2021-07-29 16:00:21 +0700

    d

---
+++ commands/invites.js
@@ -0,0 +1,159 @@
+const Profile = require("../models/profileSchema");
+
+module.exports = {
+  name: "invites",
+  description: "invites commands",
+  execute(client, message, args, Discord, profileData) {
+    if (message.guild.id == "869141664529272842") {
+      const user = message.author;
+      const invited = async () => {
+        const invites = await message.guild.fetchInvites();
+        let userInv = invites.filter(
+          (u) => u.inviter && u.inviter.id === user.id
+        );
+        if (userInv.size <= 0) {
+          return message.channel.send(
+            `${user.username} don't have any invites`
+          );
+        }
+        let invCodes = userInv.map((x) => x.code).join(",");
+        let i = 0;
+        userInv.forEach((inv) => (i += inv.uses));
+        if (i > profileData.invites) {
+          const prof = await Profile.findOneAndUpdate(
+            { userID: profileData.userID },
+            { invites: i }
+          );
+          if (10 > i && i >= 3) {
+            const prof = await Profile.findOneAndUpdate(
+              { userID: profileData.userID },
+              { $inc: { tokens: 150 } }
+            );
+            const newEmbed = new Discord.MessageEmbed()
+              .setColor("#304281")
+              .setTitle(`You reached ${i} invites!`)
+              .setAuthor(
+                message.author.username,
+                message.author.displayAvatarURL({
+                  format: "png",
+                  dynamic: true,
+                })
+              )
+              .setDescription(
+                `You have won 150 tokens. Reach 10 invites for your next reward!`
+              )
+              .setFooter("visit https://getmeow.gg/tokens to get tokens!")
+              .setURL("https://getmeow.gg/tokens");
+            message.channel.send(newEmbed);
+          }
+          if (20 > i && i >= 10) {
+            const prof = await Profile.findOneAndUpdate(
+              { userID: profileData.userID },
+              { $inc: { tokens: 200 } }
+            );
+            const newEmbed = new Discord.MessageEmbed()
+              .setColor("#304281")
+              .setTitle(`You reached ${i} invites!`)
+              .setAuthor(
+                message.author.username,
+                message.author.displayAvatarURL({
+                  format: "png",
+                  dynamic: true,
+                })
+              )
+              .setDescription(
+                `You have won 200 tokens. Reach 20 invites for your next reward!`
+              )
+              .setFooter("visit https://getmeow.gg/tokens to get tokens!")
+              .setURL("https://getmeow.gg/tokens");
+            message.channel.send(newEmbed);
+          }
+          if (35 > i && i >= 20) {
+            const prof = await Profile.findOneAndUpdate(
+              { userID: profileData.userID },
+              { $inc: { tokens: 500 } }
+            );
+            const newEmbed = new Discord.MessageEmbed()
+              .setColor("#304281")
+              .setTitle(`You reached ${i} invites!`)
+              .setAuthor(
+                message.author.username,
+                message.author.displayAvatarURL({
+                  format: "png",
+                  dynamic: true,
+                })
+              )
+              .setDescription(
+                `You have won 500 tokens. Reach 35 invites for your next reward!`
+              )
+              .setFooter("visit https://getmeow.gg/tokens to get tokens!")
+              .setURL("https://getmeow.gg/tokens");
+            message.channel.send(newEmbed);
+          }
+          if (60 > i && i >= 35) {
+            const prof = await Profile.findOneAndUpdate(
+              { userID: profileData.userID },
+              { $inc: { tokens: 1000 } }
+            );
+            const newEmbed = new Discord.MessageEmbed()
+              .setColor("#304281")
+              .setTitle(`You reached ${i} invites!`)
+              .setAuthor(
+                message.author.username,
+                message.author.displayAvatarURL({
+                  format: "png",
+                  dynamic: true,
+                })
+              )
+              .setDescription(
+                `You have won 1000 tokens. Reach 60 invites for your next reward!`
+              )
+              .setFooter("visit https://getmeow.gg/tokens to get tokens!")
+              .setURL("https://getmeow.gg/tokens");
+            message.channel.send(newEmbed);
+          }
+          if (i >= 60) {
+            const prof = await Profile.findOneAndUpdate(
+              { userID: profileData.userID },
+              { $inc: { tokens: 1800 }, invites: "10000001" }
+            );
+            1;
+            const newEmbed = new Discord.MessageEmbed()
+              .setColor("#304281")
+              .setTitle(`You reached ${i} invites!`)
+              .setAuthor(
+                message.author.username,
+                message.author.displayAvatarURL({
+                  format: "png",
+                  dynamic: true,
+                })
+              )
+              .setDescription(`You have won 1800 tokens. Thanks for your help!`)
+              .setFooter("visit https://getmeow.gg/tokens to get tokens!")
+              .setURL("https://getmeow.gg/tokens");
+            message.channel.send(newEmbed);
+          }
+        } else {
+          const newEmbed = new Discord.MessageEmbed()
+            .setColor("#304281")
+            .setTitle(`You currently have ${i} invites.`)
+            .setAuthor(
+              message.author.username,
+              message.author.displayAvatarURL({
+                format: "png",
+                dynamic: true,
+              })
+            )
+            .setDescription(
+              `3 invites: 150 Tokens, 10 invites: 200 Tokens, 20 invites: 500 Tokens, 35 invites: 1000 Tokens, 60 invites: 1800 Tokens. `
+            )
+            .setFooter("visit https://getmeow.gg/tokens to get tokens!")
+            .setURL("https://getmeow.gg/tokens");
+          message.channel.send(newEmbed);
+        }
+      };
+      invited();
+    }
+  },
+};
+
