a9ce242725f43a999bdaca33dcf14ac0f3d6684e
Jongbin Won <jongbinwon@192.168.2.7>
2021-08-02 11:14:23 +0700

    d

--- commands/invites.js
+++ commands/invites.js
@@ -8,6 +8,7 @@ module.exports = {
       const user = message.author;
       const invited = async () => {
         const invites = await message.guild.fetchInvites();
+        console.log(invites, user)
         let userInv = invites.filter(
           (u) => u.inviter && u.inviter.id === user.id
         );
@@ -24,7 +25,9 @@ module.exports = {
             { userID: profileData.userID },
             { invites: i }
           );*/
+          console.log(i, profileData.invites)
           if (10 > i && i >= 3 && 3 >= profileData.invites) {
+            console.log('hey1')
             const prof = await Profile.findOneAndUpdate(
               { userID: profileData.userID },
               { $inc: { tokens: 150 }, invites: i }
@@ -47,6 +50,7 @@ module.exports = {
             message.channel.send(newEmbed);
           }
           if (20 > i && i >= 10 && 10 >= profileData.invites) {
+            console.log('hey2')
             const prof = await Profile.findOneAndUpdate(
               { userID: profileData.userID },
               { $inc: { tokens: 200 }, invites: i }
@@ -69,6 +73,7 @@ module.exports = {
             message.channel.send(newEmbed);
           }
           if (35 > i && i >= 20 && 20 >= profileData.invites) {
+            console.log('hey3')
             const prof = await Profile.findOneAndUpdate(
               { userID: profileData.userID },
               { $inc: { tokens: 500 }, invites: i }
@@ -91,6 +96,7 @@ module.exports = {
             message.channel.send(newEmbed);
           }
           if (60 > i && i >= 35 && 35 >= profileData.invites) {
+            console.log('hey4')
             const prof = await Profile.findOneAndUpdate(
               { userID: profileData.userID },
               { $inc: { tokens: 1000 }, invites: i }
@@ -113,6 +119,7 @@ module.exports = {
             message.channel.send(newEmbed);
           }
           if (i >= 60) {
+            console.log('hey5')
             const prof = await Profile.findOneAndUpdate(
               { userID: profileData.userID },
               { $inc: { tokens: 1800 }, invites: "10000001" }
@@ -133,7 +140,27 @@ module.exports = {
               .setURL("https://getmeow.gg/tokens");
             message.channel.send(newEmbed);
           }
+          else{
+            console.log('hey6')
+          const newEmbed = new Discord.MessageEmbed()
+            .setColor("#304281")
+            .setTitle(`You currently have ${i} invites.`)
+            .setAuthor(
+              message.author.username,
+              message.author.displayAvatarURL({
+                format: "png",
+                dynamic: true,
+              })
+            )
+            .setDescription(
+              `3 invites: 150 Tokens, 10 invites: 200 Tokens, 20 invites: 500 Tokens, 35 invites: 1000 Tokens, 60 invites: 1800 Tokens. `
+            )
+            .setFooter("visit https://getmeow.gg/tokens to get tokens!")
+            .setURL("https://getmeow.gg/tokens");
+          message.channel.send(newEmbed);
+          }
         } else {
+          console.log('hey6')
           const newEmbed = new Discord.MessageEmbed()
             .setColor("#304281")
             .setTitle(`You currently have ${i} invites.`)
