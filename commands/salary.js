40cbc697940e69a66b70c8e4f4df5c14afad3490
Jongbin Won <jongbinwon@Jongbins-MBP.lan>
2022-02-13 21:36:35 -0800

    d

---
+++ commands/salary.js
@@ -0,0 +1,29 @@
+const Profile = require("../models/profileSchema");
+
+module.exports = {
+    name: "salary",
+    description: "salary",
+    execute(client, message, args, Discord, profileData) {
+    if(profileData.lastTransaction == "yes"){
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor("#304281")
+        .setTitle(`Weekly Salary Already Paid!`)
+        .setURL("https://churro.gg/");
+      message.channel.send(newEmbed);
+    }   else{
+        const portfolio = Profile.findOneAndUpdate(
+            {
+              customerID: profileData.customerID,
+            },
+               { $inc: { tokens: +500 }, lastTransaction: "yes"  }
+          , (req, res) => {
+          });
+          const newEmbed = new Discord.MessageEmbed()
+        .setColor("#304281")
+        .setTitle(`Your Weekly Salary Paid!`)
+        .setURL("https://churro.gg/");
+      message.channel.send(newEmbed);
+    }
+      
+    },
+  };
